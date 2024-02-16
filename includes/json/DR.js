55b6b7677f9953846324928e4e11d8cf003fcc02
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-02-16 16:01:21 +0700

    Update DR.js

--- includes/json/DR.js
+++ includes/json/DR.js
@@ -1,128 +1,39 @@
 var dailies = {
 	"daily-side": {
-        task: "Adventure Seeker",
+        task: "Cười thường xuyên",
         url: "#",
         short: true,
-        desc: "Complete side quests from adventure seeker",
-        img: "placeholder",
+        desc: "Duy trì việc cười thường xuyên",
         boxcount: 1
     },
     "daily-battlegrounds": {
-        task: "Battleground",
+        task: "Thở sâu",
         time: "8-12-18-22",
         duration: 2,
         url: "#",
         short: true,
-        desc: "Repeatable 3 times for rewards.",
-        img: "placeholder",
-        boxcount: 3
+        desc: "Thở sâu liên tục 10 lần để nạp đầy oxy cho cơ thể",
+        boxcount: 10
     },
 	"daily-bestiary": {
-        task: "Bestiary",
+        task: "Nín thở không hơn",
         url: "#",
         short: true,
-        desc: "Complete 3 Bestiary pages",
-        img: "placeholder",
-        boxcount: 3
+        desc: "Thực hiện nín thở không hơi trong thời gian lâu nhất",
+        boxcount: 5
     },
     "daily-bounty": {
-        task: "Bounties",
+        task: "Nín thở đầy hơi",
         url: "#",
         short: true,
-        desc: "Complete 8 Bounties (can stack up to 24 if not all are completed in a day)",
-        img: "placeholder",
-        boxcount: 8
+        desc: "Thực hiện nín thở đầy hơi trong thời gian lâu nhất",
+        boxcount: 5
     },
     "daily-faction": {
-        task: "Contracts Daily",
+        task: "Phát âm OM",
         url: "#",
         short: true,
-        desc: "Complete your daily Shadow Contract (can stack up to 5)",
-        img: "placeholder",
-        boxcount: 1
-    },
-    "daily-challenge-reward": {
-        task: "Daily Challenge Rewards",
-        url: "#",
-        short: true,
-        desc: "Claim the daily challenge rewards at the Challenge Rift",
-        img: "placeholder",
-        boxcount: 1
-    },
-    "daily-elder": {
-        task: "Elder Rift",
-        url: "#",
-        short: true,
-        desc: "Collect 3 Daedessa's Blessing rewards from elder rifts",
-        img: "placeholder",
-        boxcount: 3
-    },
-    "daily-first": {
-        task: "First Kill of The Day",
-        url: "#",
-        short: true,
-        desc: "Defeat at least one monster a day to earn a free reward.",
-        img: "placeholder",
-        boxcount: 1
-    },
-    "daily-reward": {
-        task: "Free Daily Rewards",
-        url: "#",
-        short: true,
-        desc: "Claim your Daily Rewards chest in the shop under Bundles.",
-        img: "placeholder",
-        boxcount: 1
-    },
-    "daily-hilt": {
-        task: "Hilts Trader Daily",
-        url: "#",
-        short: true,
-        desc: "Buy items from Hilts Trader - Changes 2 x daily",
-        img: "hilts",
-        boxcount: 1
-    },
-    "daily-sanctuary": {
-        task: "Legacy of the Horadrim",
-        url: "#",
-        short: true,
-        desc: "Run Iben's Sanctuary and open chests",
-        img: "key",
-        boxcount: 1
-    },
-    "daily-crest": {
-        task: "Rare Crest",
-        url: "#",
-        short: true,
-        desc: "Collect a free {rare_crest} from elder rift",
-        img: "rare_crest",
-        boxcount: 1
-    },
-	"daily-lottery": {
-        task: "Shadow Lottery",
-        time: "12-18-21",
-        duration: 1,
-        url: "#",
-        short: true,
-        desc: "Enter the Shadow Lottery.",
-        img: "signet",
-        boxcount: 1
-    },
-	"assembly": {
-        task: "The Assembly",
-        time: "19",
-        duration: 2,
-        day: "1-2-3-4-5-6",
-        url: "#",
-        desc: "Collect all 4 blessings.",
-        img: "assembly",
-        boxcount: 4
-    },
-    "daily-zone": {
-        task: "Zone Event",
-        url: "#",
-        short: true,
-        desc: "Run world events that occur on specific days such as Ancient Nightmare and Haunted Carriage",
-        img: "placeholder",
-        boxcount: 1
+        desc: "Tập phát âm OM trong thời gian dài nhất",
+        boxcount: 5
     },
 };
