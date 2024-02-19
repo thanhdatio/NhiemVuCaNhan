baf0d7658ee3d2332f26cc408d7e87c013b273ac
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-02-19 09:38:42 +0700

    Update DD.js

--- includes/json/DD.js
+++ includes/json/DD.js
@@ -6,25 +6,18 @@ var dailydrops = {
         desc: "Rà soát kỹ lại khối lượng 21 làn đảm báo đúng đủ",
         boxcount: 10
     },
-    "daily-lair": {
-        task: "Truy cập Facebook cập nhật thông tin",
-        url: "https://www.facebook.com",
+    "chayquangcao": {
+        task: "Học chạy quảng cáo",
+        url: "#",
         short: true,
-        desc: "Cập nhật các thông tin trên Facebook, cố gắng xử lý thông tin trong thời gian ngắn nhất",
+        desc: "Nghiên cứu cách chạy quảng cáo hiệu quả, muốn không mất thời gian thì phải dùng tiền để chạy quảng cáo",
         boxcount: 6
     },
-    "daily-lair1": {
+    "kiemtrathitruong": {
         task: "Kiểm tra thị trường",
         url: "#",
         short: true,
         desc: "Xem thông tin thị trường, xem nhanh nhất có thể và tìm hội",
         boxcount: 3
-    },
-    "daily-lair2": {
-        task: "Mua ETHW",
-        url: "#",
-        short: true,
-        desc: "Mua tích lũy ETHW hàng ngày trên OKX, xử lý nhanh gọn nhanh nhất có thể",
-        boxcount: 1
     },
 };
