3b19f5181df26638d5e9126ee6aefd68f6374647
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-02-19 14:47:04 +0700

    Update DD.js

--- includes/json/DD.js
+++ includes/json/DD.js
@@ -11,20 +11,20 @@ var dailydrops = {
         url: "#",
         short: true,
         desc: "Nghiên cứu cách chạy quảng cáo hiệu quả, muốn không mất thời gian thì phải dùng tiền để chạy quảng cáo",
-        boxcount: 6
+        boxcount: 10
     },
     "kiemtrathitruong": {
         task: "Kiểm tra thị trường",
         url: "#",
         short: true,
         desc: "Xem thông tin thị trường, xem nhanh nhất có thể và tìm hội",
-        boxcount: 3
+        boxcount: 10
     },
     "timduanchayaff": {
         task: "Tiếp thị liên kết các sàn tiền mã hóa",
         url: "#",
         short: true,
         desc: "Tìm tên miền hợp lý, thiết kế các lading page các sàn lớn. Sau đó chạy quảng cáo.",
-        boxcount: 3
+        boxcount: 10
     },
 };
