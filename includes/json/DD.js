51569387fb004b24a0094afa5b12e0091e771e7e
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-05-08 09:12:56 +0700

    Update DD.js

--- includes/json/DD.js
+++ includes/json/DD.js
@@ -1,30 +1,24 @@
 var dailydrops = {
-    "khoiluong21lan": {
-        task: "Rà soát khối lượng 21 làn",
+    "dexuatits": {
+        task: "Hoàn thiện đề xuất nâng cấp ITS",
         url: "",
         short: true,
-        desc: "Rà soát kỹ lại khối lượng 21 làn đảm báo đúng đủ",
+        desc: "Dự thảo báo cáo TCT nêu rõ trước mắt cần thực hiện những gì để hệ thống ITS có thể hoạt động được",
         boxcount: 10
     },
-    "chayquangcao": {
-        task: "Học chạy quảng cáo",
+    "quytrinhbaotri": {
+        task: "Nghiên cứu quy trình bảo trì",
         url: "#",
         short: true,
-        desc: "Nghiên cứu cách chạy quảng cáo hiệu quả, muốn không mất thời gian thì phải dùng tiền để chạy quảng cáo",
+        desc: "Đánh giá khả năng có thể thuyết trình hướng dẫn các điểm khác trong quy trình bảo trì bản mới",
         boxcount: 10
     },
-    "kiemtrathitruong": {
-        task: "Kiểm tra thị trường",
+    "thaoluanits": {
+        task: "Kế hoạch thảo luận ITS",
         url: "#",
         short: true,
-        desc: "Xem thông tin thị trường, xem nhanh nhất có thể và tìm hội",
+        desc: "Dự thảo và thông báo kế hoạch thảo luận ITS",
         boxcount: 10
-    },
-    "timduanchayaff": {
-        task: "Làm lading page tiếp thị dịch vụ trên accesstrade",
-        url: "#",
-        short: true,
-        desc: "Tạo lading page các dịch vụ CPA, chạy quảng cáo popads để thử nghiệm.",
-        boxcount: 10
-    },
+    }
+    
 };
