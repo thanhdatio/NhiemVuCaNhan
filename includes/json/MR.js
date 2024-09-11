60e5f72dedebb40ba71a9234b92f233eeaedc03e
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-09-11 09:35:12 +0700

    Update MR.js

--- includes/json/MR.js
+++ includes/json/MR.js
@@ -1,8 +1,44 @@
 var monthlies = {
-    "hilt": {
-        task: "Tổng kết tuần",
-        url: "#",
-        desc: "Tổng kết kiểm điểm công việc hàng tuần",
-        boxcount: 4
+    "so1": {
+        task: "Báo cáo hệ thống ITS sau mưa bão - 13/9",
+        url: "",
+        short: true,
+        desc: "Tổng hợp hiện trạng hệ thống sau mưa bão ảnh hưởng những gì, cần đề xuất thực hiện thế nào",
+        boxcount: 10
     },
+    "so2": {
+        task: "Đề cương và dự toán TVGS lập BC KTKT 16/9",
+        url: "#",
+        short: true,
+        desc: "Trình đề cương và khái toán tạm tính nhiệm vụ TVGS lập báo cáo KTKT",
+        boxcount: 10
+    },
+    "so3": {
+        task: "Kế hoạch và dự toán đo suy hao 20/9",
+        url: "#",
+        short: true,
+        desc: "Trình kế hoạch và dự toán thực hiện đo suy hao cáp quang",
+        boxcount: 10
+    },
+    "so4": {
+        task: "Trình lựa chọn TVGS lập BCKTKT 25/9",
+        url: "#",
+        short: true,
+        desc: "Trình lựa chọn được đơn vị khả thi lập BCKTKT",
+        boxcount: 10
+    },
+    "so5": {
+        task: "Trình lựa chọn đơn vị đo suy hao 27/9",
+        url: "#",
+        short: true,
+        desc: "Trình lựa chọn được đơn vị khả thi thực hiện đo suy hao cáp quang",
+        boxcount: 10
+    },
+    "so6": {
+        task: "Kế hoạch và dự toán đo suy hao 30/9",
+        url: "#",
+        short: true,
+        desc: "Trình quysets định phê duyệt và giao việc thực hiện đo suy hao cáp quang",
+        boxcount: 10
+    }
 };
