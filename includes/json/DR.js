3f38c1d510caef889653483a92e2aeafbbaa5616
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-09-20 08:54:48 +0700

    Update DR.js

--- includes/json/DR.js
+++ includes/json/DR.js
@@ -1,39 +1,39 @@
 var dailies = {
-	"daily-side": {
+	"cuoinhieu": {
         task: "Cười thường xuyên",
         url: "#",
         short: true,
         desc: "Duy trì việc cười thường xuyên, giữ ánh mắt kiên định",
         boxcount: 10
     },
-    "daily-battlegrounds": {
-        task: "Thở sâu",
+    "thosau": {
+        task: "Thở sâu liên tục",
         time: "8-12-18-22",
         duration: 2,
         url: "#",
         short: true,
-        desc: "Thở sâu liên tục 10 lần để nạp đầy oxy cho cơ thể",
+        desc: "Thở sâu liên tục ít nhất 10 lần để nạp đầy oxy cho cơ thể",
         boxcount: 10
     },
-	"daily-bestiary": {
+	"ninthokykhi": {
         task: "Nín thở không hơi",
         url: "#",
         short: true,
-        desc: "Thực hiện nín thở không hơi trong thời gian lâu nhất",
+        desc: "Thực hiện nín thở không hơi trong thời gian ít nhất 1 phút",
         boxcount: 10
     },
     "daily-bounty": {
         task: "Nín thở đầy hơi",
         url: "#",
         short: true,
-        desc: "Thực hiện nín thở đầy hơi trong thời gian lâu nhất",
+        desc: "Thực hiện nín thở đầy hơi trong thời gian ít nhất 1 phút 30 giây",
         boxcount: 10
     },
     "daily-faction": {
         task: "Phát âm OM",
         url: "#",
         short: true,
-        desc: "Tập phát âm OM trong thời gian dài nhất",
+        desc: "Tập phát âm OM trong thời gian ít nhất 30 giây",
         boxcount: 10
     },
     "keothatlung": {
